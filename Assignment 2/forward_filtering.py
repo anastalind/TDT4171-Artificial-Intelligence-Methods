eebe4a4bf48cd862facbdaa4d420fe70bca06ac4
Anastasia <anastal@stud.ntnu.no>
2020-02-12 19:51:33 +0100

    Done with assignment. Had to change forward function to reuse it in part c

--- Assignment 2/forward_filtering.py
+++ Assignment 2/forward_filtering.py
@@ -1,38 +1,62 @@
 import numpy as np
 
-# Function which implements filtering using the FORWARD operation
-# as described in equations 15.5 and 15.12 in 'Artifical Intelligence - A Modern Approach'
-# @param numpy array T - Conditional probability matrix.
-# @param numpy array evidences - Array of evidence of size t.
-# @param int t - Number of time steps.
-# @return numpy array f - The probabilty of rain on day t.
-def forward(T, evidences, t):
-    # Initial value for f_1:0
-    if t == 0:
-        return np.array([0.5, 0.5])
+def normalize(element):
+    return (1/np.sum(element)) * element
 
-    # Finding the right observation conditional probabilty matrix based on evidence
-    if evidences[t - 1] == True:
-        O = np.array([[0.9, 0.0], [0.0, 0.2]])
-    else:
-        O = np.array([[0.1, 0.0], [0.0, 0.8]])
+def forward(T, O, f, ev):
+    return normalize(np.dot(O[ev], np.dot(np.transpose(T), f)))
 
-    # Calculating f_1:t recursively, as the forward function returns f_1:t-1
-    f = np.dot(O, np.dot(np.transpose(T), forward(T, evidences, t-1)))
+def backward(T, O, b, ev):
+    return np.dot(T, np.dot(O[ev], b))
 
-    # Normalization constant
-    alpha = 1 / np.sum(f)
+def filtering(T, O, ev, prior):
+    f = [prior]
+    t = len(ev)
 
-    f = alpha * f
+    for i in range(t):
+        f.append(forward(T, O, f[i], ev[i]))
+        #print("Forward message ", i, ":", f[i])
 
     return f
 
+def smoothing(T, O, ev, prior):
+    f = filtering(T, O, ev, prior)
+    b = np.array([1.0, 1.0])
+    s = []
+
+    t = len(ev)
+
+    for i in range(t - 1, -1, -1):
+        s.append(normalize(np.multiply(f[i + 1], b)))
+        b = backward(T, O, b, ev[i])
+
+        #print("Backward message ", i, ": ", b)
+    return s
+
 
 if __name__ == "__main__":
-    T = np.array([[0.7, 0.3], [0.3, 0.7]])
+    # State matrix
+    T = np.array([
+        [0.7, 0.3],
+        [0.3, 0.7]
+        ])
 
-    evidences_a = np.array([True, True])
-    forward(T, evidences_a, 2)
+    # Observation matrix
+    O = np.array([
+        [[0.1, 0.0],
+         [0.0, 0.8]],
 
-    evidences_b = np.array([True, True, False, True, True])
-    forward(T, evidences_b, 5)
+        [[0.9, 0.0],
+         [0.0, 0.2]]
+    ])
+    ev2 = np.array([True, True])
+    ev5 = np.array([True, True, False, True, True])
+    prior = np.array([0.5, 0.5])
+
+    # Part B
+    filtering(T, O, ev2, prior)[len(ev2) - 1]
+    filtering(T, O, ev5, prior)
+
+    # Part C
+    smoothing(T, O, ev2, prior)[len(ev2) - 1]
+    smoothing(T, O, ev5, prior)
